f2bc1b735df19ca6d540d678b11fc29f2cd19a01
Volkova-D-O <Volkova.d.o@mail.ru>
2025-11-08 20:56:48 +0500

    ready to work in docker

--- src/main/java/ru/wteam/SpringUserCRUD/SpringUserCrudMavenWebappApplication.java
+++ src/main/java/ru/wteam/SpringUserCRUD/SpringUserCrudMavenWebappApplication.java
@@ -1,18 +1,26 @@
 package ru.wteam.SpringUserCRUD;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 
+import io.github.cdimascio.dotenv.Dotenv;
 
 @EnableKafka
 @SpringBootApplication(scanBasePackages = "ru.wteam")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SpringUserCrudMavenWebappApplication {
+	Dotenv dotenv = Dotenv.load();
+//	Dotenv dotenv = Dotenv.configure().directory("app/.env").load();
 
-	public static void main(String[] args) {
+
+
+
+		public static void main(String[] args) {
 		SpringApplication.run(SpringUserCrudMavenWebappApplication.class, args);
 	}
 
